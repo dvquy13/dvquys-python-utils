8ae20dbee707f0d0eae01e7be40d4f8a20cb322a
quy.dinh3195@gmail.com <quydv6@cyberspace.vn>
2018-05-17 19:02:20 +0700

    minor

--- sklearn/model_selection.py
+++ sklearn/model_selection.py
@@ -24,19 +24,22 @@ CLASSIFIERS = {
 
 
 def spotcheck(classifiers=CLASSIFIERS, X=None, y=None, score='roc_auc', cv=3, sort_by='mean'):
+    results = {}
     for clf in classifiers.values():
-        results = {}
         clf_name = clf.__class__.__name__
         print("---\nSpotchecking for {}".format(clf_name))
-        scores = cross_val_score(estimator=clf, X=X, y=y, scoring=score, cv=cv)
+        try:
+            scores = cross_val_score(estimator=clf, X=X, y=y, scoring=score, cv=cv)
+        except Exception as e:
+            print(e.message)
+            continue
         results[clf_name] = (
         scores.mean(), scores.std(), scores.mean() - scores.std() * 2, scores.mean() + scores.std() * 2)
-        print("---")
-        print(pd.DataFrame
-                .from_dict(results, orient='index')
-                .rename(columns={0: 'mean',
-                           1: 'std',
-                           2: 'lower_bound',
-                           3: 'upper_bound'})
-                .sort_values(sort_by, ascending=False))
-        return results
+    print("---")
+    results_df = pd.DataFrame.from_dict(results, orient='index') \
+        .rename(columns={0: 'mean',
+                         1: 'std',
+                         2: 'lower_bound',
+                         3: 'upper_bound'}) \
+        .sort_values(sort_by, ascending=False)
+    return results_df
