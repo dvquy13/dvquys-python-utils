cf8816db9da2bc3c7f041df0e36373ea70b8f27d
quydv1 <quydv1@hybris-powerbi2.adayroi.local>
2018-08-14 15:59:48 +0700

    add timeutils

---
+++ python_core/timeutils.py
@@ -0,0 +1,19 @@
+import time
+import sys
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print('{} runtime: {:.0f}s'.format(method.__name__, (te - ts)))
+        return result
+    return timed
+
+def update_progress(job_title, progress):
+    length = 40 # modify this to change the length
+    block = int(round(length*progress))
+    msg = "\r{0}: [{1}] {2}%".format(job_title, "#"*block + "-"*(length-block), round(progress*100, 2))
+    if progress >= 1: msg += " DONE\r\n"
+    sys.stdout.write(msg)
+    sys.stdout.flush()
