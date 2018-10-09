879b3fc60a6f04b3401722381f713d13a9accd9b
dvquy <dvquy.13@gmail.com>
2018-10-09 23:23:25 +0700

    init visualize folder with seaborn pairplots util func

---
+++ visualize/multivariate.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn-ticks')
+
+from src.utils import preprocess_utils
+
+class PairViz():
+    @staticmethod
+    def pairplot(data: pd.DataFrame, cols: list, remove_outlier: bool = True, hue: str = None, **kwargs):
+        if remove_outlier:
+            plot_data = preprocess_utils.ZscoreOutlierHandler.remove_outliers(data, cols, return_original_df=True)
+        else:
+            plot_data = data
+        sns.pairplot(
+            plot_data,
+            vars=cols,
+            hue=hue,
+            palette="muted",
+            plot_kws={"s": 15},
+            diag_kind='kde',
+            height=4,
+            **kwargs
+        )
+        plt.show()
